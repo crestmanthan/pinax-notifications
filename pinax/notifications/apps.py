b3f1c307329a6e815b28be0fbe90a3a202a4d900
crestmanthan <manthan.b@crestinfosystems.net>
2022-06-30 11:04:50 +0530

    first commit

---
+++ pinax/notifications/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig as BaseAppConfig
+# from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
+
+
+class AppConfig(BaseAppConfig):
+
+    name = "pinax.notifications"
+    label = "pinax_notifications"
+    verbose_name = _("Pinax Notifications")
